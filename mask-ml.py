3eae1ccb5b9f6b4ac3f6fce9c04eabb0fd90a18d
Balaji Anbalagan <vijibalaji2003@gmail.com>
2023-11-23 15:05:45 +0530

    mask model

--- mask-ml.py
+++ mask-ml.py
@@ -6,27 +6,26 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("keras_model.h5", compile=False)
+model = load_model("mask-model.h5", compile=False)
 
 # Load the labels
-class_names = open("labels.txt", "r").readlines()
+class_names = open("mask-labels.txt", "r").readlines()
 
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FPS, 5)
+
 while True:
     # Grab the webcamera's image.
     ret, image = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
-    image1 = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Show the image in a window
-    # image = cv2.putText(image, 'OpenCV', org, font,  
-    #                fontScale, color, thickness, cv2.LINE_AA) 
+    cv2.imshow("Webcam Image", image)
 
     # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image1, dtype=np.float32).reshape(1, 224, 224, 3)
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
 
     # Normalize the image array
     image = (image / 127.5) - 1
@@ -40,24 +39,7 @@ while True:
     # Print prediction and confidence score
     print("Class:", class_name[2:], end="")
     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-    font = cv2.FONT_HERSHEY_SIMPLEX 
-  
-    # org 
-    org = (50, 50) 
-    
-    # fontScale 
-    fontScale = 1
-    
-    # Blue color in BGR 
-    color = (255, 0, 0) 
-    
-    # Line thickness of 2 px 
-    thickness = 2
-    
-    # Using cv2.putText() method 
-    image1 = cv2.putText(image1,  class_name[2:], org, font,  
-                   fontScale, color, thickness, cv2.LINE_AA) 
-    cv2.imshow("Webcam Image", image1)
+
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
 
